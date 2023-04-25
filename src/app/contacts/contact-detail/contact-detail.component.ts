d5380305c55dde70dc052c8aef1f80a37b08fa76
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2023-04-25 14:53:20 -0700

    Finished instructions for W02 Assignment

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent {
+  contact: Contact;
+
+  constructor() {
+    this.contact = new Contact(
+      "",//"1",
+      "",//"R. Kent Jackson",
+      "",//"jacksonk@byui.edu",
+      "",//"208-496-3771",
+      "",//"../../assets/images/jacksonk.jpg",
+      null
+    );
+  }
+}
---
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'cms-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent {
+  contacts: Contact[];
+
+  constructor() {
+    this.contacts = [
+      {
+        id: "1",
+        name: "R. Kent Jackson",
+        email: "jacksonk@byui.edu",
+        phone: "208-496-3771",
+        imageUrl: "../../assets/images/jacksonk.jpg",
+        group: null
+      },
+      {
+        id: "2",
+        name: "Rex Barzee",
+        email: "barzeer@byui.edu",
+        phone: "208-496-3768",
+        imageUrl: "../../assets/images/barzeer.jpg",
+        group: null
+      }
+    ];
+  }
+}
